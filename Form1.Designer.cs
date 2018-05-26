0024416510fb25154f86f4cff61e6cc2df337eb2
rdomanski <rdomanski94@gmail.com>
2018-05-26 11:34:25 +0200

    dodanie rysowania przyciskow odpowiadajacym polom w labiryncie o okreslonym rozmiarze

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(359, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 346);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,43 @@ namespace IDAStar
 {
     public partial class Form1 : Form
     {
+        Grid grid;
+        Button[] buttons;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x = textBox1.Text;
+            int wymiar = Int32.Parse(x);
+            grid = new Grid(wymiar);
+            buttons = new Button[wymiar*wymiar];
+            int index = 0;
+            for(int i = 0; i < wymiar; i++)
+            {
+                for(int j = 0; j < wymiar; j++)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(30, 30);
+                    b.Location = new Point(i * 30, j * 30);
+                    b.Text = i + " " + j;
+                    b.Tag = grid.getWezel(i, j);
+                    b.Click += new EventHandler(buttonClick);
+                    buttons[index++] = b;
+                }
+            }
+            panel1.Controls.AddRange(buttons);
+        }
+
+        protected void buttonClick(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            Wezel wezel = (Wezel)b.Tag;
+            textBox1.Text = wezel.x + " " + wezel.y;
+            
         }
     }
 }
